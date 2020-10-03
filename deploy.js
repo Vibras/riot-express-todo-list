3d6aa3ae50156bdea766cd52c4912eb4e19cb80b
Ricky <rheredia420@gmail.com>
2020-10-03 17:45:27 -0600

    updated node-ssh in deploy.js

--- deploy.js
+++ deploy.js
@@ -2,8 +2,9 @@ var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
-const {NodeSSH} = require('node-ssh')
-ssh = new NodeSSH();
+node_ssh = require('node-ssh');
+ssh = new node_ssh();
+
 
 // the method that starts the deployment process
 function main() {
